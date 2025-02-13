de6fa7b61d716d70e4fdbb2213f272812063b9a2
Leon <leonbalteriano@gmail.com>
2025-02-13 10:28:46 +0100

    subject

--- MailItem.java
+++ MailItem.java
@@ -13,6 +13,8 @@ public class MailItem
     private String to;
     // The text of the message.
     private String message;
+    
+    private String subject;
 
     /**
      * Create a mail item from sender to the given recipient,
@@ -21,15 +23,16 @@ public class MailItem
      * @param to The intended recipient of this item.
      * @param message The text of the message to be sent.
      */
-    public MailItem(String from, String to, String message)
+    public MailItem(String from, String to, String subject, String message)
     {
         this.from = from;
         this.to = to;
+        this.subject = subject;
         this.message = message;
     }
     
     public String getSubject() {
-        return from;
+        return subject;
     }
     
     
@@ -65,6 +68,7 @@ public class MailItem
     {
         System.out.println("From: " + from);
         System.out.println("To: " + to);
+        System.out.println("Subject: " + subject);
         System.out.println("Message: " + message);
     }
 }
